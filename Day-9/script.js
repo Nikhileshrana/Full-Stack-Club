33d5e007caccc85540db17693ff7b3b0d29f0716
Nikhilesh Rana <realnikhileshrana@gmail.com>
2024-03-06 15:20:41 +0530

    Learning Javascript

---
+++ Day-9/script.js
@@ -0,0 +1,29 @@
+document.getElementById("demo").innerHTML = "Hello Mitrr";
+document.getElementById("demo").style.background = "black";
+document.getElementById("demo").style.color = "orange";
+
+
+console.log("True");
+
+
+
+var var_check = 10;
+let namert = 35;
+nikhil [5] = ("hello" , "hi");
+
+console.log(nikhil[1]);
+const huehue = "hemlo";
+
+console.log(var_check);
+
+console.log(huehue);
+
+console.log(namert);
+
+
+
+
+//   Writing into an HTML element, using innerHTML.
+//   Writing into the HTML output using document.write().
+//   Writing into an alert box, using window.alert().
+//   Writing into the browser console, using console.log()
